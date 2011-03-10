e40955ff86e94b29eaf53a727e11645454881066
Martin-Louis Bright <mlbright@gmail.com>
2011-03-09 20:46:56 -0500

    minor

--- pyd.py
+++ pyd.py
@@ -11,25 +11,31 @@ def flatten(listOfLists):
     "Flatten one level of nesting"
     return chain.from_iterable(listOfLists)
 
+
 if __name__ == "__main__":
 
     rects = []
     lines = sys.stdin.readlines()
     for line in lines[1:]:
         x,y = line.split()
-        rects.append([int(x), int(y)])
+        x,y = int(x),int(y)
+        if y > x:
+            x,y = y,x
+        rects.append((x,y))
 
-    min_area = sum([x*y for (x,y) in rects])
-    largest_dimension = max(flatten(rects))
-    other_dimension = min_area / largest_dimension
+    min_area            = sum([x*y for (x,y) in rects])
+    largest_dimension   = max([ x for x,y in rects ])
+    other_dimension     = min_area / largest_dimension
 
     packer = CygonRectanglePacker(largest_dimension,other_dimension)
     fits = True
-    for r in rects:
-        point = packer.tryFindBestPlacement(r[0],r[1])
+    for x,y in rects:
+        point = packer.tryFindBestPlacement(x,y)
         if point is None:
             fits = False
             break
+        else:
+            print "%d %d" % (point.x, point.y)
 
     if fits:
         print "%d" % (min_area)
